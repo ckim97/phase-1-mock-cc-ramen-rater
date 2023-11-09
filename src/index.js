320ce49575f11acbb4e5e8ec1a8dc80f8d42f5c7
Christopher <chrisjwkim97@gmail.com>
2023-11-08 21:30:28 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,65 +1,61 @@
-// write your code here
-document.addEventListener("DOMContentLoaded", function(){
-    fetch("http://localhost:3000/ramens")
-        .then(response => response.json())
-        .then(data => {
-            let ramenContainer = document.getElementById("ramen-menu");
-            for (let ramen of data) {
-                let image = document.createElement("img");
-                image.src = ramen.image;
-                image.addEventListener("click", function(){
-                    let detailedImage = document.querySelector(".detail-image");
-                    detailedImage.src = ramen.image;
+fetch("http://localhost:3000/ramens")
+    .then(res => res.json())
+    .then(data => {
+        for (let ramen of data) {
+            appendImage(ramen);
+        }
+    })
 
-                   let name = document.querySelector(".name");
-                   name.textContent = ramen.name;
-
-                   let restaurant = document.querySelector(".restaurant");
-                   restaurant.textContent = ramen.restaurant;
-
-                    let rating = document.getElementById("rating-display");
-                    rating.textContent = ramen.rating;
-
-                    let comment = document.getElementById("comment-display");
-                    comment.textContent = ramen.comment;
-
-                });
-            ramenContainer.append(image);
-            };
-        });
+function appendImage(ramenObject) {
+    let ramenContainer = document.querySelector("#ramen-menu");
     
+    let image = document.createElement("img");
+    image.src = ramenObject.image;
+
+    ramenContainer.append(image);
     
-})
+    image.addEventListener("click", function() {
+        renderDetails(ramenObject);
+    });
+}
 
-let formSubmission = document.getElementById("new-ramen");
-formSubmission.addEventListener("submit", function(e){
-    e.preventDefault();
-    let ramenContainer = document.getElementById("ramen-menu");
-    let newImage = document.createElement("img");
-    newImage.src = e.target.image.value;
+function renderDetails(ramenObject) {
+    let image = document.querySelector(".detail-image");
+    image.src = ramenObject.image;
 
-    ramenContainer.append(newImage);
+    let name = document.querySelector(".name");
+    name.textContent = ramenObject.name;
 
-    formSubmission.reset();
-})
+    let restaurant = document.querySelector(".restaurant");
+    restaurant.textContent = ramenObject.restaurant;
+
+    let rating = document.querySelector("#rating-display");
+    rating.textContent = ramenObject.rating;
+
+    let comment = document.querySelector("#comment-display");
+    comment.textContent = ramenObject.comment;
+}
+
+let form = document.querySelector("#new-ramen");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+    e.preventDefault()
+    let newName = e.target["name"].value;
+    let newRestaurant = e.target["restaurant"].value;
+    let newImage = e.target["image"].value;
+    let newRating = e.target["rating"].value;
+    let newComment = e.target["new-comment"].value;
 
 
+    let newRamen = {
+        name: newName,
+        restaurant: newRestaurant,
+        image: newImage,
+        rating: newRating,
+        comment: newComment
+    };
 
-document.addEventListener("DOMContentLoaded", function(){
-    let restaurants = [];
-    fetch("http://localhost:3000/ramens")
-        .then(response => response.json())
-        .then(data => function(){
-            for (let ramen of data) {
-                restaurants.push(ramen);
-            }
-            let detailedImage = document.querySelector(".detail-image");
-            detailedImage.src = restaurants[0].image;
-
-            let name = document.querySelector(".name");
-            name.textContent = restaurants[0].name;
-
-            let restaurant = document.querySelector(".restaurant");
-            restaurant.textContent = restaurants[0].restaurant;
-        })
-})
+    appendImage(newRamen);
+    form.reset();
+}
